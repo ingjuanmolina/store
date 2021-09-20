ed07582497cbb10230d425ae3f502752a894f9c3
juancarlosmolinaglobant <juancarlos.molina@globant.com>
2021-09-19 19:32:29 -0500

    Offers implementation

--- src/main/java/com/coding/task/store/model/Order.java
+++ src/main/java/com/coding/task/store/model/Order.java
@@ -13,6 +13,8 @@ public class Order {
     private final Map<String, Item> chart;
     private final List<Item> totalItems;
     private BigDecimal totalValue;
+    private BigDecimal totalDiscount;
+    private BigDecimal totalValueWithDiscount;
 
     public Order() {
         this.chart = new HashMap<>();
@@ -27,17 +29,40 @@ public class Order {
 
     private void processOrder() {
         BigDecimal total = BigDecimal.ZERO;
+        BigDecimal discount = BigDecimal.ZERO;
         for (Item item : chart.values()) {
+
+
                 if (item.getQuantity() > 0) {
                 this.totalItems.add(item);
                 total = total.add(item.getBaseGood().getPrice().multiply(BigDecimal.valueOf(item.getQuantity())));
+                discount = discount.add(getTotalWithOffer(item));
             }
         }
         setTotalValue(total);
+        setValueWithDiscount(discount);
+        setTotalDiscount();
+    }
+
+    private BigDecimal getTotalWithOffer(Item item) {
+        int baseQuantityToSetPrice = 0;
+        if (item.getBaseGood().getDescription().equalsIgnoreCase("orange")) {
+            baseQuantityToSetPrice = item.getQuantity() - item.getQuantity() / 2;
+        }
+
+        if (item.getBaseGood().getDescription().equalsIgnoreCase("apple")) {
+            baseQuantityToSetPrice = (item.getQuantity() / 3) * 2 + item.getQuantity() % 3;
+        }
+
+        return item.getBaseGood().getPrice().multiply(BigDecimal.valueOf(baseQuantityToSetPrice));
     }
 
     private void setTotalValue(BigDecimal value) {
         this.totalValue = value;
+    }
+
+    private void setValueWithDiscount(BigDecimal value) {
+        this.totalValueWithDiscount = value;
     }
 
     private void validateAndSaveItem(Entry entry) {
@@ -71,5 +96,17 @@ public class Order {
 
     public BigDecimal getTotalValue() {
         return totalValue;
+    }
+
+    public BigDecimal getTotalDiscount() {
+        return totalDiscount;
+    }
+
+    public BigDecimal getTotalValueWithDiscount() {
+        return totalValue.subtract(totalDiscount);
+    }
+
+    public void setTotalDiscount() {
+        this.totalDiscount = totalValue.subtract(totalValueWithDiscount);
     }
 }
