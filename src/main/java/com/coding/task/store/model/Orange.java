3de4914bd9e58dfc36d60a55379eea22402a0230
ingjuanmolina <ing.jcmolina@gmail.com>
2021-09-24 22:10:47 -0500

    Old models removed

--- src/main/java/com/coding/task/store/model/Orange.java
+++
@@ -1,9 +0,0 @@
-package com.coding.task.store.model;
-
-import java.math.BigDecimal;
-
-public class Orange extends BaseGood{
-    public Orange() {
-        super("Orange", BigDecimal.valueOf(0.25));
-    }
-}
